1d88a25dd259396e859611da9d8d15c946d5db50
Simon <simonxd@gmail.com>
2015-10-16 14:48:32 -0300

    implemented when system

--- src/scripts/behaviors/navigation.js
+++ src/scripts/behaviors/navigation.js
@@ -40,8 +40,6 @@ exports.properties = {
 };
 
 exports.ready = function () {
-
-    console.log('qwejqwkjehqwke');
     // Auxiliary object for navigation within fields
     this.navigator = {
         previousQuestion: this.previousQuestion.bind(this),
@@ -55,22 +53,40 @@ exports.ready = function () {
 exports.previousQuestion = function () {
 
     if (this.get('isAtFirstQuestion')) {
-        alert('you are just starting, c\'mon!');
-    } else {
-        // not the last, go on!
-        this.set('currentQuestionIndex', this.get('currentQuestionIndex') - 1);
+        console.warn('carbo-inquirer already at first question');
+
+        return;
     }
 
+    var previousIndex = _getPreviousQuestionIndex.call(this);
+
+    _gotoQuestion.call(this, previousIndex);
 };
 
 exports.nextQuestion = function () {
 
     if (this.get('isAtLastQuestion')) {
-        alert('you have completed the stuff');
-    } else {
-        // not the last, go on!
-        this.set('currentQuestionIndex', this.get('currentQuestionIndex') + 1);
+        console.warn('carbo-inquirer already at last question');
+
+        return;
     }
+
+    // not the last, go on!
+    var question = _getCurrentQuestion.call(this);
+    
+    // validate
+    _validateAnswer.call(this, question)
+        .then(function (validation) {
+            if (validation.valid) {    
+
+                // get the next question
+                var nextIndex = _getNextQuestionIndex.call(this);
+
+                _gotoQuestion.call(this, nextIndex);
+            } else {
+                alert(validation.errorMessage);
+            }
+        }.bind(this));
 };
 
 /**
@@ -82,8 +98,6 @@ exports._handleCurrentQuestionIndexChange = function (currentQuestionIndex, last
     var isLast  = false;
 
     if (this.questions) {
-
-
         var current = this.get('currentQuestionIndex');
         // check if the current question is the first one
         isFirst = current === 0;
@@ -94,3 +108,111 @@ exports._handleCurrentQuestionIndexChange = function (currentQuestionIndex, last
     this.set('isAtFirstQuestion', isFirst);
     this.set('isAtLastQuestion', isLast);
 };
+
+/**
+ * Auxiliary functions
+ */
+
+/**
+ * Navigates to the given question object
+ */
+function _gotoQuestion(index) {
+    this.set('currentQuestionIndex', index);
+}
+
+/**
+ * Moves the question index a given delta
+ * @param  {Number} delta
+ */
+function _deltaQuestion(delta) {
+    this.set('currentQuestionIndex', this.get('currentQuestionIndex') + delta);
+}
+
+/**
+ * Retrieves the current question
+ * @return {QuestionObject}
+ */
+function _getCurrentQuestion() {
+    var qs = this.get('questions');
+
+    return qs[this.get('currentQuestionIndex')];
+}
+
+/**
+ * Validates a question answer
+ * @param  {QuestionObject} question that needs validation
+ */
+function _validateAnswer(question) {
+    var answer  = question.answer;
+    var answers = this.readAnswers();
+
+    // object containing the validation results
+    var validation = {
+        valid: true,
+    };
+
+    if (typeof question.validate === 'function') {
+        return Q.when(question.validate(answer, answers)).then(function (errorMessage) {
+            if (errorMessage) {
+                validation.valid = false;
+                validation.errorMessage = errorMessage;
+            }
+
+            return validation;
+        });
+    } else {
+        return Q(validation);
+    }
+}
+
+/**
+ * Verifies if a given question has to be asked to the user
+ * @param  {QuestionObject}  question [description]
+ * @return {Boolean}          [description]
+ */
+function _isQuestionNeeded(question) {
+    var answers = this.readAnswers();
+    var needed = true;
+
+    if (typeof question.when === 'function') {
+        needed = question.when(answers);
+    }
+
+    return needed;
+}
+
+/**
+ * Loops through possible next questions
+ * and retrieves the immediate next one.
+ *
+ * Skips all questions whose `.when` method returns false
+ * @param  {Number} from Optional index from which start looking for the next question
+ * @return {Number}      Index
+ */
+function _getNextQuestionIndex(fromIndex) {
+    fromIndex = arguments.length === 1 ? fromIndex : this.get('currentQuestionIndex');
+
+    var questions = this.get('questions');
+
+    // look for questions starting fromIndex the next one
+    questions = questions.slice(fromIndex + 1, questions.length);
+
+    return fromIndex + 1 + _.findIndex(questions, _isQuestionNeeded.bind(this));
+}
+
+/**
+ * Loops through possible previous questions
+ * and retrieves the immediate previous one.
+ *
+ * Skips all questions whose `.when` method returns false
+ * @param  {Number} from Optional index from which start looking for the next question
+ * @return {Number}      Index
+ */
+function _getPreviousQuestionIndex(fromIndex) {
+    fromIndex = arguments.length === 1 ? fromIndex : this.get('currentQuestionIndex');
+
+    // look for questions fromIndex start until the current one
+    var questions = this.get('questions').slice(0, fromIndex);
+
+    return _.findLastIndex(questions, _isQuestionNeeded.bind(this));
+}
